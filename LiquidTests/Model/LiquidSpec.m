c64e3f0730dbf25aa5f9902f66f554bc408e1442
Miguel M. Almeida <miguelcma@gmail.com>
2014-06-21 21:03:31 +0100

    [Tests] for commit 187233e: sync identifyUser:
    Signed-off-by: Miguel M. Almeida <miguelcma@gmail.com>

--- LiquidTests/Model/LiquidSpec.m
+++ LiquidTests/Model/LiquidSpec.m
@@ -22,51 +22,79 @@ describe(@"Liquid", ^{
         [LQDevice stub:@selector(releaseVersion) andReturn:@"9.8"];
 
         [Liquid softReset];
-        [Liquid sharedInstanceWithToken:@"12345678901234567890abcdef"];
-        [[Liquid sharedInstance] identifyUser];
-        [[Liquid sharedInstance] setSessionTimeout:1];
-        [NSThread sleepForTimeInterval:1.0f];
     });
 
     describe(@"applicationDidBecomeActive:", ^{
-        context(@"given an app that is in background for less than the timeout time", ^{
-            __block NSString *previousUserId;
-            __block NSString *previousSessionId;
-
-            beforeEach(^{
-                previousUserId = [[Liquid sharedInstance] userIdentifier];
-                previousSessionId = [[Liquid sharedInstance] sessionIdentifier];
-
-                [NSThread sleepForTimeInterval:0.1f];
-                [[Liquid sharedInstance] applicationWillResignActive:nil];
-                [NSThread sleepForTimeInterval:0.25f];
-                [[Liquid sharedInstance] applicationDidBecomeActive:nil];
-                [NSThread sleepForTimeInterval:0.1f];
+        context(@"given a Liquid singleton", ^{
+            beforeAll(^{
+                [Liquid sharedInstanceWithToken:@"12345678901234567890abcdef"];
+                [[Liquid sharedInstance] identifyUser];
+                [[Liquid sharedInstance] setSessionTimeout:1];
+                [NSThread sleepForTimeInterval:1.0f];
             });
 
-            it(@"should keep the previous Session ID", ^{
-                [[[[Liquid sharedInstance] sessionIdentifier] should] equal:(NSString *)previousSessionId];
+            context(@"given an app that is in background for less than the timeout time", ^{
+                __block NSString *previousUserId;
+                __block NSString *previousSessionId;
+
+                beforeEach(^{
+                    previousUserId = [[Liquid sharedInstance] userIdentifier];
+                    previousSessionId = [[Liquid sharedInstance] sessionIdentifier];
+
+                    [NSThread sleepForTimeInterval:0.1f];
+                    [[Liquid sharedInstance] applicationWillResignActive:nil];
+                    [NSThread sleepForTimeInterval:0.25f];
+                    [[Liquid sharedInstance] applicationDidBecomeActive:nil];
+                    [NSThread sleepForTimeInterval:0.1f];
+                });
+
+                it(@"should keep the previous Session ID", ^{
+                    [[[[Liquid sharedInstance] sessionIdentifier] should] equal:(NSString *)previousSessionId];
+                });
+            });
+
+            context(@"given an app that is in background for more than the timeout time", ^{
+                __block NSString *previousUserId;
+                __block NSString *previousSessionId;
+
+                beforeEach(^{
+                    previousUserId = [[Liquid sharedInstance] userIdentifier];
+                    previousSessionId = [[Liquid sharedInstance] sessionIdentifier];
+
+                    [NSThread sleepForTimeInterval:0.1f];
+                    [[Liquid sharedInstance] applicationWillResignActive:nil];
+                    [NSThread sleepForTimeInterval:2.0f];
+                    [[Liquid sharedInstance] applicationDidBecomeActive:nil];
+                    [NSThread sleepForTimeInterval:0.1f];
+                });
+
+                it(@"should create a new Session ID", ^{
+                    [[[[Liquid sharedInstance] sessionIdentifier] shouldNot] equal:(NSString *)previousSessionId];
+                });
             });
         });
+    });
 
-        context(@"given an app that is in background for more than the timeout time", ^{
-            __block NSString *previousUserId;
-            __block NSString *previousSessionId;
+    describe(@"track:", ^{
+        it(@"it should auto identify User with the auto identifier (device ID)", ^{
+            Liquid *liquidInstance = [[Liquid alloc] initWithToken:@"abcdef123456"];
+            [liquidInstance track:@"openApplication"];
+            [[liquidInstance.userIdentifier should] equal:liquidInstance.deviceIdentifier];
+        });
+    });
 
-            beforeEach(^{
-                previousUserId = [[Liquid sharedInstance] userIdentifier];
-                previousSessionId = [[Liquid sharedInstance] sessionIdentifier];
+    describe(@"identifyUserWithIdentifier:", ^{
+        it(@"it should identify User with the correct identifier", ^{
+            Liquid *liquidInstance = [[Liquid alloc] initWithToken:@"abcdef123456"];
+            [liquidInstance identifyUserWithIdentifier:@"john"];
+            [[liquidInstance.userIdentifier should] equal:@"john"];
+        });
 
-                [NSThread sleepForTimeInterval:0.1f];
-                [[Liquid sharedInstance] applicationWillResignActive:nil];
-                [NSThread sleepForTimeInterval:2.0f];
-                [[Liquid sharedInstance] applicationDidBecomeActive:nil];
-                [NSThread sleepForTimeInterval:0.1f];
-            });
-            
-            it(@"should create a new Session ID", ^{
-                [[[[Liquid sharedInstance] sessionIdentifier] shouldNot] equal:(NSString *)previousSessionId];
-            });
+        it(@"after 1 second it should keep the correct identifier", ^{
+            Liquid *liquidInstance = [[Liquid alloc] initWithToken:@"abcdef123456"];
+            [liquidInstance identifyUserWithIdentifier:@"john"];
+            [NSThread sleepForTimeInterval:1.0f];
+            [[liquidInstance.userIdentifier should] equal:@"john"];
         });
     });
 });
