95333faf07fdfea59888e9d1fa9bf149a415f245
Miguel M. Almeida <miguelcma@gmail.com>
2015-11-03 15:57:24 +0000

    [Release] New Liquid iOS SDK release: v2.0.0-rc1
    Signed-off-by: Miguel M. Almeida <miguelcma@gmail.com>

--- Liquid/LQDefaults.h
+++ Liquid/LQDefaults.h
@@ -8,7 +8,7 @@
 
 #import <Foundation/Foundation.h>
 
-#define kLQVersion @"1.2.1"
+#define kLQVersion @"2.0.0-rc1"
 #define kLQBundle @"io.lqd.ios"
 #define kLQDevicePlatform @"iOS"
 #define kLQNotificationLQDidReceiveValues @"io.lqd.ios.Notifications:LQDidReceiveValues"
